967aca6c60164e69843d278a78b034816f0fc248
7lucky-axolotl228 <7error228@gmail.com>
2023-05-31 15:04:04 +0500

    Add href to links and if in js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 const elModalOpener = document.querySelector('.js-modal-opener');
 const elModal = document.querySelector('.modal');
 
-elModalOpener.addEventListener('click', function() {
-  elModal.classList.add('modal-open');
-});
+if (elModalOpener) {
+  elModalOpener.addEventListener('click', function() {
+    elModal.classList.add('modal-open');
+  });
+}
 
-const elModalBack = elModal.querySelector('.js-modal-close');
-const elModalContentButton = elModal.querySelector('.js-modal-content-open-button');
 
-elModalBack.addEventListener('click', function() {
-  elModal.classList.remove('modal-open');
-});
-elModalContentButton.addEventListener('click', function() {
-  elModal.classList.remove('modal-open');
-});
+const elModalBack = document.querySelector('.js-modal-close');
+const elModalContentButton = document.querySelector('.js-modal-content-open-button');
+
+if (elModalBack) {
+  elModalBack.addEventListener('click', function() {
+    elModal.classList.remove('modal-open');
+  });
+}
+
+if (elModalContentButton) {
+  elModalContentButton.addEventListener('click', function() {
+    elModal.classList.remove('modal-open');
+  });
+}
